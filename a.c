0b6e5bb739346293f5dcc1b3b374d36b04c7bfe8
Thbop <75861321+thbop@users.noreply.github.com>
2025-01-11 12:55:03 -0500

    Basic collisions + game reset

--- a.c
+++ a.c
@@ -26,23 +26,25 @@ void DrawChar(unsigned char *screen, char c, int x, int y) {
     
 
 void DrawRect( unsigned char *screen, char c, int x, int y, int width, int height ) {
-    // for ( int j = 0; j < height; j++ ) {
-    //     for ( int i = 0; i < width; i++ ) {
-    //         DrawChar(c, x+i, y+j);
-    //     }
-    // }
     for ( int j = 0; j < height; j++ ) {
         for ( int i = 0; i < width; i++ ) {
-            DrawChar(screen, '#', x+i, y+j);
+            DrawChar(screen, c, x+i, y+j);
         }
     }
 }
 
+unsigned char CollisionPointRect( float x, float y, int rx, int ry, int rwidth, int rheight ) { // I would normally package vec2s and rects, but not here
+    return rx <= x && x <= rx+rwidth &&
+           ry <= y && y <= ry+rheight;
+}
+
 // Game specific
+#define GRAVITY              0.1f
+#define RESET_DELAY          500
+
 #define FLAPPY_GLYPH         '%'
 #define FLAPPY_X             30
-#define FLAPPY_FLAP_ACC      0.5f;
-#define GRAVITY              0.1f;
+#define FLAPPY_FLAP_ACC      0.5f
 
 #define PIPE_GLYPH           '#'
 #define NUM_OF_PIPES         5
@@ -58,22 +60,43 @@ struct {
     float y, vy;
 } flappy;
 
+void ResetFlappy() {
+    flappy.vy = 0.0f;
+    flappy.y = SCREEN_HEIGHT*0.5f;
+}
+
 typedef struct {
     float x;
     int openingY;
 } pipe;
 
+// Cheeky, ik
+#define GetPipeTopRect(p) (int)p.x, 0, PIPE_WIDTH, p.openingY
+#define GetPipeBottomRect(p) (int)p.x, (p.openingY+PIPE_GAP_SIZE), PIPE_WIDTH, SCREEN_HEIGHT-(p.openingY+PIPE_GAP_SIZE)
+
 void DrawPipe(unsigned char *screen, pipe p) {
-    DrawRect(screen, PIPE_GLYPH, (int)p.x, 0, PIPE_WIDTH, p.openingY);
-    DrawRect(screen, PIPE_GLYPH, (int)p.x, (p.openingY+PIPE_GAP_SIZE), PIPE_WIDTH, SCREEN_HEIGHT-(p.openingY+PIPE_GAP_SIZE));
+    DrawRect(screen, PIPE_GLYPH, GetPipeTopRect(p));
+    DrawRect(screen, PIPE_GLYPH, GetPipeBottomRect(p));
 
 }
 
 void NewPipe( pipe *p ) {
     p->x = SCREEN_WIDTH;
-    p->openingY = PIPE_TD_PADDING + rand() % (SCREEN_HEIGHT-PIPE_TD_PADDING);
+    p->openingY = PIPE_TD_PADDING + rand() % (SCREEN_HEIGHT-PIPE_TD_PADDING-PIPE_GAP_SIZE);
 }
 
+void ResetPipes( pipe *pipes, int *nextPipe ) {
+    for ( int i = 1; i < NUM_OF_PIPES; i++ )
+        pipes[i].x = -1.0f;
+    *nextPipe = 0;
+    NewPipe(pipes); // Essentially &pipes[0]
+}
+
+void Reset( pipe *pipes, int *nextPipe ) {
+    Sleep(RESET_DELAY);
+    ResetFlappy();
+    ResetPipes(pipes, nextPipe);
+}
 
 int mainCRTStartup() {
     // Initialize screen
@@ -82,17 +105,12 @@ int mainCRTStartup() {
     DWORD dwBytesWritten;
     SetConsoleActiveScreenBuffer(hConsole);
 
-    // Flappy
-    flappy.vy = 0.0f;
-    // flappy.cooldown = 0;
-    flappy.y = SCREEN_HEIGHT*0.5f;
-
-    // Pipes
     pipe pipes[NUM_OF_PIPES];
-    for ( int i = 1; i < NUM_OF_PIPES; i++ )
-        pipes[i].x = -1.0f;
-    int nextPipe = 0;
-    NewPipe(pipes); // Essentially &pipes[0]
+    int nextPipe;
+
+
+    Reset(pipes, &nextPipe);
+    
 
     unsigned char running = true;
     while ( running ) {
@@ -105,15 +123,27 @@ int mainCRTStartup() {
 
         flappy.y += flappy.vy;
 
+        if ( flappy.y >= SCREEN_HEIGHT )
+            Reset(pipes, &nextPipe);
+
         ClearScreen();
 
         // Pipes
         for ( int i = 0; i < NUM_OF_PIPES; i++ ) {
+            // Pipe movement and drawing
             if ( pipes[i].x > 0.0f ) {
                 pipes[i].x -= PIPE_SPEED;
                 DrawPipe(screen, pipes[i]);
             }
+            // Check collisions
+            if (
+                CollisionPointRect(FLAPPY_X, flappy.y, GetPipeTopRect(pipes[i])) ||
+                CollisionPointRect(FLAPPY_X, flappy.y, GetPipeBottomRect(pipes[i]))
+            ) {
+                Reset(pipes, &nextPipe);
+            }
         }
+        // Cycle pipes
         if ( pipes[nextPipe].x < SCREEN_WIDTH-PIPE_X_SPACING ) {
             if ( nextPipe < NUM_OF_PIPES-1 ) nextPipe++;
             else                             nextPipe = 0;
