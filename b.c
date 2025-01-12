0502786bdf4d2ec8ec850e278903dce341c7efcc
Thbop <75861321+thbop@users.noreply.github.com>
2025-01-11 20:02:17 -0500

    Lines are still rough, but looking good

--- b.c
+++ b.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "engine.h"
 #include "line.h"
@@ -25,8 +26,23 @@ vec3 vec3MultiplyValue(vec3 p, float v) {
     return (vec3){ p.x * v, p.y * v, p.z * v };
 }
 
+vec3 vec3Rotate(vec3 p, float theta, int axis) {
+    float
+        sin_theta = sinf(theta),
+        cos_theta = cosf(theta);
+    switch (axis) {
+        case 0: return (vec3){ p.x, p.y*cos_theta-p.z*sin_theta, p.y*sin_theta+p.z*cos_theta };
+        case 1: return (vec3){ p.x*cos_theta + p.z*sin_theta, p.y, -p.x*sin_theta + p.z*cos_theta };
+        case 2: return (vec3){ p.x*cos_theta - p.y*sin_theta, p.x*sin_theta + p.y*cos_theta, p.z };
+    }
+}
+
+// vec3 vec3Multiply(vec3 p, vec3 q) {
+//     return (vec3){ p.x * q.x, p.y * q.y, p.z * q.z };
+// }
+
 vec3 vec3Add(vec3 p, vec3 q) {
-    return(vec3){ p.x + q.x, p.y + q.y, p.z + q.z };
+    return (vec3){ p.x + q.x, p.y + q.y, p.z + q.z };
 }
 
 struct {
@@ -83,18 +99,22 @@ void GenerateCubeMesh(mesh *m) {
     m->quadBuffer[5]  = (quad){ 1, 2, 6, 5 };          // Right
 }
 
+void RotateMesh(mesh *m, float theta, int axis) {
+    for ( size_t v = 0; v < m->vertBufferSize; v++ )
+        m->vertBuffer[v] = vec3Rotate(m->vertBuffer[v], theta, axis);
+}
+
 void RenderMesh(Surface *screen, char c, mesh *m) {
-    for ( int q = 0; q < m->quadBufferSize; q++ ) {
-        vec2 v0 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id0], m->scale ), m->origin ) );
-        vec2 v1 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id1], m->scale ), m->origin ) );
-        vec2 v2 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id2], m->scale ), m->origin ) );
-        vec2 v3 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id3], m->scale ), m->origin ) );
+    for ( size_t q = 0; q < m->quadBufferSize; q++ ) {
+        vec2 pv0 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id0], m->scale ), m->origin ) );
+        vec2 pv1 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id1], m->scale ), m->origin ) );
+        vec2 pv2 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id2], m->scale ), m->origin ) );
+        vec2 pv3 = projectToCamera( vec3Add( vec3MultiplyValue( m->vertBuffer[m->quadBuffer[q].id3], m->scale ), m->origin ) );
 
-        DrawLineV(screen, c, v0, v1);
-        DrawLineV(screen, c, v1, v2);
-        DrawLineV(screen, c, v2, v3);
-        DrawLineV(screen, c, v3, v0);
-
+        DrawLineV(screen, c, pv0, pv1);
+        DrawLineV(screen, c, pv1, pv2);
+        DrawLineV(screen, c, pv2, pv3);
+        DrawLineV(screen, c, pv3, pv0);
     }
 }
 
@@ -108,17 +128,19 @@ int mainCRTStartup() {
 
     mesh cube;
     GenerateCubeMesh(&cube);
-    cube.origin = (vec3){ 0.0f, 0.0f, 100.0f };
-    cube.scale  = 10.0f;
-
+    cube.origin   = (vec3){ 0.0f, 0.0f, 100.0f };
+    cube.scale    = 16.0f;
     
     while ( true ) {
         ClearScreen(screen);
-
+        RotateMesh(&cube, 0.01f, 0);
+        RotateMesh(&cube, 0.03f, 1);
+        RotateMesh(&cube, 0.02f, 2);
         RenderMesh(screen, '#', &cube);
+
         
         // vec2 q = projectToCamera( vec3Add( vec3MultiplyValue( cube.vertBuffer[4], cube.scale ), cube.origin ) );
-        // wsprintf(screen+SCREEN_WIDTH+1, "%d %d", (int)q.x, (int)q.y);
+        // wsprintf(screen+SCREEN_WIDTH+1, "%d %d %d", (int)p.x, (int)p.y, (int)p.z);
         // DrawChar(screen, '@', (int)q.x, (int)q.y);
 
         UpdateConsole(screen); // Update screen
